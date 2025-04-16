79fa4a55bd04f7a35a8f8b9b8d021cc808501af4
Elif <43elif1305@gmail.com>
2025-04-16 23:35:16 +0300

    dakika kolu eklenir ve saat calisir duruma getirilir

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function updateClock() {
     const now = new Date();
 
     const seconds = now.getSeconds();
-
+    const minutes = now.getMinutes();
     const hours = now.getHours();
 
     const secDeg = (seconds / 60) * 360;
-
+    const minDeg = (minutes / 60) * 360 + (seconds / 60) * 6;
     const hourDeg = (hours % 12 / 12) * 360 + (minutes / 60) * 30;
 
     secondHand.style.transform = `translateX(-50%) rotate(${secDeg}deg)`;
-
+    minuteHand.style.transform = `translateX(-50%) rotate(${minDeg}deg)`;
     hourHand.style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
 }
 

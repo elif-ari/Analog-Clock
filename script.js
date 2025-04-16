bd3e60354e1923949ec6e9c0b6d5bc996f287b76
Elif <43elif1305@gmail.com>
2025-04-16 23:31:49 +0300

    saat kolu eklenir

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const secondHand = document.getElementById('secondHand');
+const secondHand = document.querySelector('.second');
+const minuteHand = document.querySelector('.minute');
+const hourHand = document.querySelector('.hour');
 
 function updateClock() {
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const degrees = (seconds / 60) * 360;
-    secondHand.style.transform = `translateX(-50%) rotate(${degrees}deg)`;
+
+    const hours = now.getHours();
+
+    const secDeg = (seconds / 60) * 360;
+
+    const hourDeg = (hours % 12 / 12) * 360 + (minutes / 60) * 30;
+
+    secondHand.style.transform = `translateX(-50%) rotate(${secDeg}deg)`;
+
+    hourHand.style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
 }
 
 setInterval(updateClock, 1000);
